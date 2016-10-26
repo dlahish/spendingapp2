e76dfccede5c989388900d029a6e3d8918689aac
Nadav Lahish <d_lahish@MacBook-Air-2.local>
2016-10-26 19:16:26 +0200

    add mail transaction function

--- app/components/settings/Fs.js
+++ app/components/settings/Fs.js
@@ -1,39 +1,77 @@
 import React, { PropTypes, Component } from 'react'
-import { Text, View, StyleSheet, ScrollView, TouchableHighlight, AlertIOS } from 'react-native'
+import { Text, View, StyleSheet, ScrollView, TouchableHighlight, AlertIOS, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import moment from 'moment'
 var RNFS = require('react-native-fs');
 
 
-  export default class Fs extends Component {
-    componentWillMount() {
-      RNFS.readDir(RNFS.MainBundlePath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
-        .then((result) => {
-          console.log('GOT RESULT', result);
+class Fs extends Component {
+    // componentWillMount() {
+    //   RNFS.readDir(RNFS.MainBundlePath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+    //     .then((result) => {
+    //       console.log('GOT RESULT', result);
+    //
+    //       // stat the first file
+    //       return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+    //     })
+    //     .then((statResult) => {
+    //       if (statResult[0].isFile()) {
+    //         // if we have a file, read it
+    //         return RNFS.readFile(statResult[1], 'utf8');
+    //       }
+    //
+    //       return 'no file';
+    //     })
+    //     .then((contents) => {
+    //       // log the file contents
+    //       console.log(contents);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err.message, err.code);
+    //     });
+    // }
 
-          // stat the first file
-          return Promise.all([RNFS.stat(result[0].path), result[0].path]);
-        })
-        .then((statResult) => {
-          if (statResult[0].isFile()) {
-            // if we have a file, read it
-            return RNFS.readFile(statResult[1], 'utf8');
-          }
+    createCsvContent = () => {
+      let fileContent = 'Category, Amount, Date, Type, Notes'+'\n'
+      let date = new Date(this.props.visibleTransactions[0].date)
 
-          return 'no file';
-        })
-        .then((contents) => {
-          // log the file contents
-          console.log(contents);
+      for (var i=0; i < this.props.visibleTransactions.length; i++) {
+        let transaction = this.props.visibleTransactions[i]
+        let formattedDate = moment(transaction.date).format('DD/MM/YYYY')
+        fileContent += `${transaction.category}, ${transaction.amount}, ${formattedDate}, ${transaction.type}, ${transaction.notes},` + '\n'
+      }
+      return fileContent
+    }
+
+    onCreatefile = () => {
+      // create a path you want to write to
+      var path = RNFS.DocumentDirectoryPath + '/test.csv';
+      var text = this.createCsvContent()
+
+      // write the file
+      RNFS.writeFile(path, text, 'utf8')
+        .then((success) => {
+          console.log('path', path);
+          console.log('FILE WRITTEN!');
         })
         .catch((err) => {
-          console.log(err.message, err.code);
+          console.log(err.message);
         });
     }
 
     render() {
       return (
-        <View>
-          <Text>File System</Text>
+        <View style={{paddingTop: 64}}>
+          <Text style={{marginBottom: 40}}>File System</Text>
+          <TouchableOpacity onPress={() => this.createCsv()}>
+            <Text>Create File</Text>
+          </TouchableOpacity>
         </View>
       )
     }
-  }
+}
+
+  export default connect(
+    (state) => ({ visibleTransactions: state.data.visibleTransactions }),
+    (dispatch) => ({})
+  )(Fs)
--- app/components/settings/Mailer.js
+++ app/components/settings/Mailer.js
@@ -2,23 +2,55 @@ import React, { PropTypes, Component } from 'react'
 import { Text, View, StyleSheet, ScrollView, TouchableHighlight, AlertIOS } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import moment from 'moment'
 import * as settingsActions from '../../actions/settings'
 var Mailer = require('NativeModules').RNMail;
+var RNFS = require('react-native-fs');
 
 class MailerPage extends Component {
+  createCsvContent = () => {
+    let fileContent = 'Category, Amount, Date, Type, Notes'+'\n'
+    let date = new Date(this.props.visibleTransactions[0].date)
+
+    for (var i=0; i < this.props.visibleTransactions.length; i++) {
+      let transaction = this.props.visibleTransactions[i]
+      let formattedDate = moment(transaction.date).format('DD/MM/YYYY')
+      fileContent += `${transaction.category}, ${transaction.amount}, ${formattedDate}, ${transaction.type}, ${transaction.notes},` + '\n'
+    }
+    return fileContent
+  }
+
+  onSendEmail = () => {
+    // create a path you want to write to
+    var path = RNFS.DocumentDirectoryPath + '/transactions.csv';
+    var text = this.createCsvContent()
+
+    // write the file
+    RNFS.writeFile(path, text, 'utf8')
+      .then((success) => {
+        console.log('path', path);
+        console.log('FILE WRITTEN!');
+        this.handleHelp()
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
   handleHelp = () => {
+    var path = RNFS.DocumentDirectoryPath + '/transactions.csv';
     Mailer.mail({
-      subject: 'need help',
+      subject: 'SpendingApp - transactions CSV',
       recipients: ['nadavlachish@gmail.com'],
       // ccRecipients: ['supportCC@example.com'],
       // bccRecipients: ['supportBCC@example.com'],
       body: 'Hello World',
       isHTML: true, // iOS only, exclude if false
-      // attachment: {
-      //   path: '',  // The absolute path of the file from which to read data.
-      //   type: '',   // Mime Type: jpg, png, doc, ppt, html, pdf
-      //   name: '',   // Optional: Custom filename for attachment
-      // }
+      attachment: {
+        path: path,  // The absolute path of the file from which to read data.
+        type: 'doc',   // Mime Type: jpg, png, doc, ppt, html, pdf
+        name: 'transactions.csv',   // Optional: Custom filename for attachment
+      }
     }, (error, event) => {
         if(error) {
           console.log('error', error)
@@ -32,7 +64,7 @@ class MailerPage extends Component {
     return (
       <View style={styles.container}>
         <TouchableHighlight
-            onPress={this.handleHelp}
+            onPress={() => this.onSendEmail()}
             underlayColor="#f7f7f7">
           <View style={{marginBottom: 20}}>
             <Text>Send an Email</Text>
@@ -59,7 +91,7 @@ const styles = {
 }
 
 export default connect(
-  (state) => ({}),
+  (state) => ({ visibleTransactions: state.data.visibleTransactions }),
   (dispatch) => ({
     actions: {
       settings: bindActionCreators(settingsActions, dispatch)
